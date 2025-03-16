139d160aabecc02fcbaadbabb655fc182c8d03c8
FrogSitWell <manhcuong25102003@gmail.com>
2025-03-16 12:09:55 +0700

    update

--- backend/main.go
+++ backend/main.go
@@ -6,12 +6,18 @@ import (
 	"urlshort/initializers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main (){
 	initializers.ConnectDB()
-
+	
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Cho phép tất cả các domain truy cập (có thể chỉnh sửa)
+		AllowMethods: "GET,POST,PUT,DELETE",
+		AllowHeaders: "Content-Type",
+	}))
 	app.Post("/shorten", controllers.ShortenURL)
 	app.Get("/:shortID", controllers.RedirectURL)
 
